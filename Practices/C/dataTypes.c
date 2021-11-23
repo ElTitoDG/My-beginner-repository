f90e03e28de2fdc9f743bf9527e3254f2631216f
ElTitoDG <43906735+Drac09@users.noreply.github.com>
2021-11-23 22:37:41 +0100

    Added C practicies

---
+++ Practices/C/dataTypes.c
@@ -0,0 +1,16 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int main () {
+    
+    int age = 40;
+    double score = 3.6;
+    char grade = 'A'; //one single character
+    char grade2[] = "A++"; //more than one character
+
+    /* printf function */
+    //printf("Hello\"World\""); rendering coutes
+    //printf("My favorite %s is: %d or %f", "number", 500); rendering strings, integer, float 
+    
+    return 0;
+}
