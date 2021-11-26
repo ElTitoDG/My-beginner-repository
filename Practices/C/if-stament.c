4d406beb6d33d5ed86e92e1628bf949705ece255
ElTitoDG <43906735+Drac09@users.noreply.github.com>
2021-11-26 23:13:54 +0100

    1.3

--- Practices/C/if-stament.c
+++ Practices/C/if-stament.c
@@ -1,19 +1,37 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-int Max(int num1, int num2) {
+int Max(int num1, int num2, int num3) {
     int result;
-    if(num1 > num2){
+    
+    if(num1 >= num2 && num1 >= num3) {
         result = num1;
-    } else {
+    } else if(num2 >= num1 && num2 >= num3){
         result = num2;
+    } else {
+        result = num3;
     }
+
+    return result;
 }
 
 
 int main() {
 
-    printf("%d", Max(4, 10));
+    if(!(3 < 2)) {
+        printf("True");
+    }
+
+    printf("%d", Max(4, 10, 15));
 
     return 0;
 }
+
+/* 
+Logical Operators: 
+    && --> AND
+    || --> OR
+    != --> NOT EQUAL
+    == --> EQUAL
+    ! --> NEG OPERATOR
+*/
