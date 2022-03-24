15517cab92434c96a8b130814b39e26e62a8c491
ElTitoDG <julianhinojosagil@gmail.com>
2022-03-24 22:20:45 +0100

    Testing libraries, needs work

---
+++ Projectos/libC_test/librarytest.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "mycode.h"
+#include <string.h>
+
+int main (int argc, char **argv)
+{
+  if (argc > 0) {
+    printf("%s\n", argv[1]);
+    printf("%s\n", reverse(argv[1]));
+  }
+  return 0;
+}
