9a8318a175da99aa1987e997ddabf631439babbd
Julián Hinojosa Gil <julianhinojosagil@MacBook-Air-de-Julian.local>
2023-01-19 10:46:56 +0100

    rust cli app

---
+++ Practices/rust/grrs/src/main.rs
@@ -0,0 +1,18 @@
+#![allow(unused)]
+
+use clap::Parser;
+
+//Search for a pattern in a file and diplay the lines that contain it
+#[derive(Parser)]
+struct Cli {
+    //The pattern to look for
+    pattern: String,
+    //The path to the file to read
+    path: std::path::PathBuf,
+}
+
+fn main() {
+
+    let args = Cli::parse();
+
+}
