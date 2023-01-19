0eeecc9d2fd77c636b0fee01f38438e8e3f5d14b
Julián Hinojosa Gil <julianhinojosagil@MacBook-Air-de-Julian.local>
2023-01-19 12:36:18 +0100

    error test

--- Practices/rust/grrs/src/main.rs
+++ Practices/rust/grrs/src/main.rs
@@ -15,4 +15,11 @@ fn main() {
 
     let args = Cli::parse();
 
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
 }
