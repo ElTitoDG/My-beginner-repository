5667bb40df8d8399cfb17c84325ffa8b7557fd0e
Julián Hinojosa Gil <julianhinojosagil@Air-de-Julian.home>
2023-01-24 22:34:01 +0100

    react project - Header finished

--- Practices/HTML 5/Computer-class/javascript/robinhood-clone/src/Header.js
+++ Practices/HTML 5/Computer-class/javascript/robinhood-clone/src/Header.js
@@ -1,23 +1,40 @@
-import React from 'react'
+import React from 'react';
 import Logo from './robinhood.svg';
+import './Header.css';
 
 function Header() {
   return (
     <div className="header__wrapper">
+
       {/* logo  */}
       <div className="header__logo">
+
         <img src={Logo} width={25}/>
-      </div>
-      {/* search */}
-      <div className="header__search">
-        <div className="header__searchContainer">
-          <input placeholder="Search" type="text"/>
-        </div>
-      </div>
-      {/*  menuitems */}
-      <div className="header__menu">
 
       </div>
+
+      {/* search */}
+      <div className="header__search">
+
+        <div className="header__searchContainer">
+
+          <input placeholder="Search" type="text"/>
+
+        </div>
+
+      </div>
+
+      {/*  menuitems */}
+      <div className="header__menuItems">
+
+        <a href="#">Free Stocks</a>
+        <a href="#">Portfolio</a>
+        <a href="#">Cash</a>
+        <a href="#">Messages</a>
+        <a href="#">Acccount</a>
+
+      </div>
+
     </div>
   )
 }
