70bdca59738f177364ed5f5755172b9aae490de0
Julián Hinojosa Gil <julianhinojosagil@Julians-MacBook-Air.local>
2023-09-22 10:45:16 +0200

    changes basic

--- Practices/C/hello.c
+++ Practices/C/hello.c
@@ -1,13 +1,13 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    
-    char characterName[] = "John";
-    int characterAge = 35;    
-    printf("There once was a man named %s\n", characterName);
-    printf("he was %d years old\n", characterAge);
-    printf("He really liked the name %s\n", characterName);
-    printf("but did no like being %d\n", characterAge);
-    return 0;
+int main() {
+  char characterName[] = "John";
+  int characterAge = 35;
+  printf("There once was a man named %s\n", characterName);
+  printf("he was %d years old\n", characterAge);
+  printf("He really liked the name %s\n", characterName);
+  printf("but did no like being %d\n", characterAge);
+  printf("Calvo Cabrón\n");
+  return 0;
 }
