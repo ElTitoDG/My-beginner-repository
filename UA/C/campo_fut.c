ff1543349b7d5513065b4fc37302d4d14ef73446
Julián Hinojosa Gil <julianhinojosagil@Julians-MacBook-Air.local>
2023-10-13 12:32:02 +0200

    update session5

--- UA/C/campo_fut.c
+++ UA/C/campo_fut.c
@@ -7,32 +7,45 @@ int main() {
   int ysize = 300;
   int xsize = 300;
 
-  int franjas, lineSize;
+  int franjas, lineas, linea;
+  char c;
 
+  printf("ysize: ");
+  scanf("%d", &ysize);
   printf("Cuantas franjas quieres: ");
   scanf("%d", &franjas);
-  printf("Tamaño de las lineas: ");
-  scanf("%d", &lineSize);
-  printf("Se dibujaran %d con un tamaño de %d pixeles\n", franjas, lineSize);
 
-  char c;
+  lineas = ysize / franjas;
+  printf("%d lineas por franja\n", lineas);
 
   // Open a new window for drawing.
   gfx_open(xsize, ysize, "Rico perez");
 
-  // Set the current drawing color to blue.
-  // gfx_color(255, 255, 255) color white.
+  // Drawing color (0, 51, 255) to blue.
+  // Drawing color (255, 255, 255) to white.
 
   // Draw a line and wait
   for (int y = 1; y < franjas; y++) {
-    gfx_line(0, y, xsize, y);
-    c = gfx_wait();
+
     if ((y % 2) == 0) {
       gfx_color(0, 51, 255);
     } else {
       gfx_color(255, 255, 255);
     }
+
+    for (int j = 1; j <= lineas; j++) {
+      gfx_line(1, linea, xsize, linea);
+      linea++;
+    }
+
+    c = gfx_wait();
   }
+
+  // while (1) {
+  //   c = gfx_wait();
+  //   if (c == 'q')
+  //     break;
+  // }
 
   return EXIT_SUCCESS;
 }
