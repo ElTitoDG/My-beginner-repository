739fd799d375e518d3268a3edf90d2631eb691b1
Julián Hinojosa Gil <julianhinojosagil@Julians-MacBook-Air.local>
2023-10-20 12:43:57 +0200

    finished practice6.01

--- UA/C/s6-triangulo.c
+++ UA/C/s6-triangulo.c
@@ -3,7 +3,9 @@
 
 void menu();
 void triII(int altura);
-
+void triSI(int altura);
+void triID(int altura);
+void triSD(int altura);
 int input();
 
 int main() {
@@ -12,23 +14,26 @@ int main() {
   menu();
   scanf("%i", &op);
 
-  h = input();
-
   switch (op) {
   case 1:
+    h = input();
     triII(h);
     printf("-------FIN-------\n");
     break;
   case 2:
-    printf("Test\n");
+    h = input();
+    triSD(h);
+    /** TODO **/
     printf("-------FIN-------\n");
     break;
   case 3:
-    printf("Test\n");
+    h = input();
+    triSI(h);
     printf("-------FIN-------\n");
     break;
   case 4:
-    printf("Test\n");
+    h = input();
+    triID(h);
     printf("-------FIN-------\n");
     break;
   case 0:
@@ -52,12 +57,48 @@ void menu() {
 }
 
 void triII(int altura) {
-
-  printf("-----------------\n");
-
-  for (int i = 1; i <= altura; i++) {
+  int i, j;
+  for (i = 1; i <= altura; i++) {
     printf("Fila %i: ", i);
-    for (int j = 1; j <= i; j++) {
+    for (j = 1; j <= i; j++) {
+      printf("x");
+    }
+    printf("\n");
+  }
+}
+
+void triSI(int altura) {
+  int i, j, k;
+  for (i = altura; i >= 1; i--) {
+    printf("Fila %i: ", i);
+    for (j = 1; j <= i; j++) {
+      printf("x");
+    }
+    printf("\n");
+  }
+}
+
+void triID(int altura) {
+  int i, j, k;
+  for (i = 1; i <= altura; i++) {
+    for (j = 0; j <= altura - i; j++) {
+      printf(" ");
+    }
+    for (k = 0; k <= altura - j; k++) {
+      printf("x");
+    }
+    printf("\n");
+  }
+}
+
+void triSD(int altura) {
+  int i, j, k;
+
+  for (i = altura; i >= 1; i--) {
+    for (j = 0; j <= altura - i; j++) {
+      printf(" ");
+    }
+    for (k = 0; k <= altura - j; k++) {
       printf("x");
     }
     printf("\n");
@@ -65,9 +106,11 @@ void triII(int altura) {
 }
 
 int input() {
+
   int altura;
   printf("Introduce la altura: ");
   scanf("%i", &altura);
+  printf("-----------------\n");
 
   return altura;
 }
