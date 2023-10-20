af3131f8a3f07391ac441a359014ec70ad6622cb
Julián Hinojosa Gil <julianhinojosagil@Julians-MacBook-Air.local>
2023-10-20 12:07:36 +0200

    base session program

--- UA/C/s6-triangulo.c
+++ UA/C/s6-triangulo.c
@@ -1,9 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void menu() {
+void menu();
+void triII(int altura);
 
-  printf("Seleccione una opcion: \n");
+int input();
+
+int main() {
+  int op, h;
+
+  menu();
+  scanf("%i", &op);
+
+  h = input();
+
+  switch (op) {
+  case 1:
+    triII(h);
+    printf("-------FIN-------\n");
+    break;
+  case 2:
+    printf("Test\n");
+    printf("-------FIN-------\n");
+    break;
+  case 3:
+    printf("Test\n");
+    printf("-------FIN-------\n");
+    break;
+  case 4:
+    printf("Test\n");
+    printf("-------FIN-------\n");
+    break;
+  case 0:
+    printf("-------FIN-------\n");
+    break;
+  }
+
+  return 0;
+}
+
+void menu() {
 
   printf("1. Triangulo Superior Izquierda\n");
   printf("2. Triangulo Superior Derecha\n");
@@ -11,31 +47,27 @@ void menu() {
   printf("4. Triangulo Inferior Derecha\n");
 
   printf("0. Exit\n");
+
+  printf("Seleccione una opcion: ");
 }
 
-int main() {
-  int sum, num1, num2;
+void triII(int altura) {
 
-  menu();
-
-  printf("Introduce dos numero entre el 1 y el 5: ");
-  scanf("%i, %i", &num1, &num2);
   printf("-----------------\n");
 
-  if (num1 >= 1 && num2 <= 5) {
-    sum = num1 + num2;
-
-    for (int i = 1; i <= sum; i++) {
-      for (int j = 1; j <= i; j++) {
-        printf("x");
-      }
-      printf("\n");
+  for (int i = 1; i <= altura; i++) {
+    printf("Fila %i: ", i);
+    for (int j = 1; j <= i; j++) {
+      printf("x");
     }
-  } else {
-    printf("Error");
-    return 1;
+    printf("\n");
   }
+}
 
-  printf("-------FIN-------\n");
-  return 0;
+int input() {
+  int altura;
+  printf("Introduce la altura: ");
+  scanf("%i", &altura);
+
+  return altura;
 }
