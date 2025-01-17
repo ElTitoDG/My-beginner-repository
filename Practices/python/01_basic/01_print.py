50928c209cbdc73da08bc72f28680ffdc3d79af4
Julián Hinojosa Gil <julianhinojosagil@gmail.com>
2025-01-18 00:02:25 +0100

    new python course

---
+++ Practices/python/01_basic/01_print.py
@@ -0,0 +1,10 @@
+# Curso de python desde cero - 01
+
+print("Hello World")
+# Las cadenas de texto pueden ser con "" o con ''
+print("Python", "es", "genial")
+
+print("Pythob", "es", "brutal", sep = "-")
+
+print("Esto se imprime", end = "!")
+print("otra frase mas")
