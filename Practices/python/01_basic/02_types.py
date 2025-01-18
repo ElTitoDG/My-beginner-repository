395c1d487eaaca4a121e029ec80bba4f2acdb445
Julián Hinojosa Gil <julianhinojosagil@gmail.com>
2025-01-18 18:54:17 +0100

    new files for python course

---
+++ Practices/python/01_basic/02_types.py
@@ -0,0 +1,7 @@
+# Tipos de datos en python
+# Con Hashtags se hacen comentarios de código
+# pero con """ -- """ se crean docs strings
+print("int: ")
+print(type(5)) # Imprime el valor del dato
+print(2345)
+print(453)
