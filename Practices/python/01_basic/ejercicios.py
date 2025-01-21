63793289907c07841c7cd2c2aa3e1397469e6d1e
Julián Hinojosa Gil <julianhinojosagil@gmail.com>
2025-01-21 20:02:37 +0100

    ejercicios completos

--- Practices/python/01_basic/ejercicios.py
+++ Practices/python/01_basic/ejercicios.py
@@ -7,7 +7,8 @@ print("\nEjercicio 1: Imprimir mensajes")
 print("Escribe un programa que imprima tu nombre y tu ciudad en líneas separadas.")
 
 ### Completa aquí
-
+print("Julián")
+print("Alicante")
 print("--------------")
 
 print("\nEjercicio 2: Muestra los tipos de datos de las siguientes variables:")
@@ -17,16 +18,20 @@ b = 3.14159
 c = "Hola mundo"
 d = True
 e = None
-
-### Completa aquí
-
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
 print("--------------")
 
 print("\nEjercicio 3: Casting de tipos")
 print("Convierte la cadena \"12345\" a un entero y luego a un float.")
 print("Convierte el float 3.99 a un entero. ¿Qué ocurre?")
 
-### Completa aquí
+print(int("12345"))
+print(float("12345"))
+print(int(3.99)) # Cuando se castea un float a entero la parte decimal del numero desaparece
 
 print("--------------")
 
@@ -35,9 +40,11 @@ print("Crea variables para tu nombre, edad y altura.")
 print("Usa f-strings para imprimir una presentación.")
 
 # "Hola! Me llamo midudev y tengo 39 años, mido 1.70 metros"
-
 ### Completa aquí
-
+name = "Julian"
+age = 20
+altura = 1.65
+print(f"Hola! Me llamo {name} y tengo {age}, mido {altura} metros.")
 print("--------------")
 
 print("\nEjercicio 5: Números")
@@ -45,3 +52,8 @@ print("1. Crea una variable con el número PI (sin asignar una variable)")
 print("2. Redondea el número con round()")
 print("3. Haz la división entera entre el número que te salió y el número 2")
 print("4. El resultado debería ser 1")
+
+PI = 3.14
+round(PI)
+num = PI/2
+print(f"Resultado: {num}")
