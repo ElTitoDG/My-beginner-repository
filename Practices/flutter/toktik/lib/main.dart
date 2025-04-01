c96c4b7bb57e2dc379fe2c25eb6cd87e57bb6064
Julián Hinojosa Gil <julianhinojosagil@gmail.com>
2025-04-01 10:59:37 +0200

    app theme

--- Practices/flutter/toktik/lib/main.dart
+++ Practices/flutter/toktik/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toktik/config/theme/app_theme.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,7 +9,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
+      title: 'TokTik',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Material App Bar'),
