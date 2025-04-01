006089a298cade279ce1a936ab67677afe2d6b8d
Julián Hinojosa Gil <julianhinojosagil@gmail.com>
2025-04-01 22:03:46 +0200

    number format + spining icon

--- Practices/flutter/toktik/lib/presentation/widgets/shared/video_buttons.dart
+++ Practices/flutter/toktik/lib/presentation/widgets/shared/video_buttons.dart
@@ -1,4 +1,6 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:toktik/config/helpers/human_formats.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 
 class VideoButtons extends StatelessWidget {
@@ -14,7 +16,14 @@ class VideoButtons extends StatelessWidget {
     return Column(
       children: [
         _CustomIconButton(value: video.likes, iconData: Icons.favorite, iconColor: Colors.red),
-        _CustomIconButton(value: video.views, iconData: Icons.remove_red_eye_outlined)
+        const SizedBox(height: 20),
+        _CustomIconButton(value: video.views, iconData: Icons.remove_red_eye_outlined),
+        const SizedBox(height: 20),
+        SpinPerfect(
+          infinite: true,
+          duration: const Duration(seconds: 5),
+          child: _CustomIconButton(value: 0, iconData: Icons.play_circle_outline)
+          )
       ],
     );
   }
@@ -39,7 +48,7 @@ class _CustomIconButton extends StatelessWidget {
           onPressed: (){}, 
           icon: Icon(iconData, color: color, size: 30)
         ),
-        Text('$value')
+        Text(HumanFormats.humanReadebleNumber(value.toDouble()))
       ],
     );
   }
