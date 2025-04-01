ac158eeb777c6fe94ab211199fbb5c17f05c0630
Julián Hinojosa Gil <julianhinojosagil@gmail.com>
2025-04-01 11:35:41 +0200

    basic provider setup

---
+++ Practices/flutter/toktik/lib/presentation/providers/discover_provider.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+
+class DiscoverProvider extends ChangeNotifier {
+  bool initialLoading = true;
+  List<VideoPost> videos = [];
+
+  Future<void> nextPage() async {
+
+    notifyListeners();
+  }
+}
