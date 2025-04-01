adb4760bbb39638dea6cef2d47487e5c64756fd1
Julián Hinojosa Gil <julianhinojosagil@gmail.com>
2025-04-01 18:30:05 +0200

    videos provider + pageview

--- Practices/flutter/toktik/lib/presentation/providers/discover_provider.dart
+++ Practices/flutter/toktik/lib/presentation/providers/discover_provider.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/infraestructure/models/local_video_model.dart';
+import 'package:toktik/share/data/local_video_post.dart';
 
 class DiscoverProvider extends ChangeNotifier {
+  // TODO: Repository
+  // TOOD: Data Source
+
   bool initialLoading = true;
   List<VideoPost> videos = [];
 
-  Future<void> nextPage() async {
+  Future<void> loadNextPage() async {
+    await Future.delayed(const Duration(seconds: 2));
 
+    final List<VideoPost> newVideos = videoPosts.map(
+      (video) => LocalVideoModel.fromJson(video).toVideoPostEntity()
+    ).toList();
+
+    videos.addAll(newVideos);
+    initialLoading = false;
     notifyListeners();
   }
 }
