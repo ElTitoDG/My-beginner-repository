26d3fb7106cfecfc1e783fcd2865d9221a2649de
Julián Hinojosa Gil <julianhinojosagil@gmail.com>
2025-04-04 19:49:43 +0200

    screens created

---
+++ Practices/flutter/widgets_app/lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InfiniteScrollScreen extends StatelessWidget {
+  static const name = 'infinite_screen';
+
+  const InfiniteScrollScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
